38833c26ba7eb290a781b22738551d26c1f353be
Cameron Deans <cdeansy@gmail.com>
2012-03-24 10:47:37 +0000

    Added domain, thumbnailURL, up votes, down votes and numComments to Thing.java

--- src/com/camsh/reddit/Thing.java
+++ src/com/camsh/reddit/Thing.java
@@ -9,14 +9,25 @@ public class Thing {
     Boolean NSFW;
     String URL;
     String author;
+    String domain;
+    String thumbnailURL;
+    int upvotes;
+    int downvotes;
+    int numComments;
 
     Thing(JSONObject object) {
         try{
+            domain = object.getString("domain");
             subreddit = object.getString("subreddit");
             title = object.getString("title");
             NSFW = object.getBoolean("over_18");
             URL = object.getString("url");
+            thumbnailURL = object.getString("thumbnail");
             author = object.getString("author");
+            numComments = object.getInt("num_comments");
+            downvotes = object.getInt("downs");
+            upvotes = object.getInt("ups");
+            
         }
         catch (Exception e) {
             e.printStackTrace();
