6cec0738d8057c59789307da1c2595fea7f92687
Cameron Deans <cdeansy@gmail.com>
2012-03-25 14:34:01 +0100

    Added a basic webView to the ThingViewFragment.

--- src/com/camsh/reddit/ThingViewFragment.java
+++ src/com/camsh/reddit/ThingViewFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 
 public class ThingViewFragment extends Fragment {
@@ -12,7 +15,13 @@ public class ThingViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.thing_view, container, false);
+       View v = inflater.inflate(R.layout.thing_view, container, false);
+        
+        WebView wv = (WebView)v.findViewById(R.id.webView1);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("http://google.com");
+
+
+       return wv;
     }
 }
