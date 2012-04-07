1feb0a0634d8a8c39bef8a0cd88c937a29d7f072
Cameron Deans <cdeansy@gmail.com>
2012-04-07 20:41:16 +0100

    Removed unneeded imports and changed LoginActivity so that DEV_LOGIN button only appears if there is a DEV_USERNAME and DEV_PASSWORD and DEV_MODE = true

--- src/com/camsh/reddit/LoginActivity.java
+++ src/com/camsh/reddit/LoginActivity.java
@@ -23,10 +23,21 @@ public class LoginActivity extends Activity {
         redditSession = new RedditSession();
 
         loginButton = (Button)findViewById(R.id.LoginButton);
-        useDevLoginButton = (Button)findViewById(R.id.UseDEV_LOGIN);
-
+        if (Constants.DEV_MODE) {
+        	useDevLoginButton = (Button)findViewById(R.id.UseDEV_LOGIN);
+        }
+        if (Constants.DEV_MODE == true) {
+	        if (Constants.DEV_USERNAME.equals("")) {
+	        	// DEV_USERNAME is missing
+	        	useDevLoginButton.setAlpha(0.0f);
+	        }
+	        if (Constants.DEV_PASSWORD.equals("")) {
+	        	// DEV_PASSWORD is missing
+	        	useDevLoginButton.setAlpha(0.0f);
+	        }
+        }
+        
         loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View view) {
                 EditText usernameInput = (EditText)findViewById(R.id.UsernameInput);
                 EditText passwordInput = (EditText)findViewById(R.id.PasswordInput);
@@ -42,21 +53,22 @@ public class LoginActivity extends Activity {
                 startActivity(intent);
             }
         });
-
-        useDevLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = Constants.DEV_USERNAME;
-                String password = Constants.DEV_PASSWORD;
-
-                RedditCookie loginCookie = redditSession.GetLoginCookie(username,password);
-                Toast toast = Toast.makeText(getApplication(),loginCookie.string, Toast.LENGTH_SHORT);
-                toast.show();
-
-                Intent intent = new Intent();
-                intent.setClass(LoginActivity.this, HomeView.class);
-                startActivity(intent);
-            }
-        });
+        if (Constants.DEV_MODE) {
+	        useDevLoginButton.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View view) {
+	                String username = Constants.DEV_USERNAME;
+	                String password = Constants.DEV_PASSWORD;
+	
+	                RedditCookie loginCookie = redditSession.GetLoginCookie(username,password);
+	                Toast toast = Toast.makeText(getApplication(),loginCookie.string, Toast.LENGTH_SHORT);
+	                toast.show();
+	
+	                Intent intent = new Intent();
+	                intent.setClass(LoginActivity.this, HomeView.class);
+	                startActivity(intent);
+	            }
+	        });
+        }
+       
     }
 }
--- src/com/camsh/reddit/SubredditListFragment.java
+++ src/com/camsh/reddit/SubredditListFragment.java
@@ -1,8 +1,6 @@
 package com.camsh.reddit;
 
-import android.app.ListActivity;
 import android.app.ListFragment;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
